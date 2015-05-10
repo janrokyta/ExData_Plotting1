a2894327e373f4384ac6dbd8418789566e8c17c2
Jan Rokyta <rokyta@atlas.cz>
2015-05-10 22:47:15 +0200

    final

--- plot1.R
+++ plot1.R
@@ -1,6 +1,7 @@
-
+Sys.setlocale("LC_ALL", "English") 
 df <- read.csv("household_power_consumption.txt", sep = ";", na.strings="?", stringsAsFactors=FALSE)
 df <- subset(df, (Date == "1/2/2007") | (Date == "2/2/2007"))
- 
-Sys.setlocale("LC_ALL", "English") 
+
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
 hist(df$Global_active_power, main = "Global Active Power", xlab="Global Active Power (kilowatts)", col="orangered")
+dev.off()
--- plot2.R
+++ plot2.R
@@ -1,8 +1,10 @@
+Sys.setlocale("LC_ALL", "English") 
 library(lubridate)
 df <- read.csv("household_power_consumption.txt", sep = ";", na.strings="?", stringsAsFactors=FALSE)
 df <- subset(df, (Date == "1/2/2007") | (Date == "2/2/2007"))
 df$DateTime <- dmy_hms(paste(df$Date,df$Time))
 
-Sys.setlocale("LC_ALL", "English") 
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
 plot(df$DateTime, df$Global_active_power, type="n", main = "", xlab="", ylab="Global Active Power (kilowatts)")
 lines(df$DateTime, df$Global_active_power, type="l") 
+dev.off()
